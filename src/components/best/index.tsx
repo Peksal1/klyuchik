63b63d1083e09010df4c5a2ebcbe7e0b1d7a2654
Peksal1 <laurs2000@gmail.com>
2023-03-09 01:15:36 +0200

    Add guild member search and filter

--- src/components/best/index.tsx
+++ src/components/best/index.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-import { List, Card, Avatar, Pagination } from "antd";
+import React, { useCallback, useEffect, useState } from "react";
+import { List, Card, Avatar, Pagination, Select, Input } from "antd";
 
 interface GuildMember {
   character: {
@@ -7,13 +7,18 @@ interface GuildMember {
     class: string;
     active_spec_name: string;
   };
+  rank: number;
 }
 
-const PAGE_SIZE = 12;
+const { Option } = Select;
+
+const PAGE_SIZE = 40;
 
 const GuildMembers: React.FC = () => {
   const [members, setMembers] = useState<GuildMember[]>([]);
   const [page, setPage] = useState<number>(1);
+  const [classFilter, setClassFilter] = useState<string>("");
+  const [searchTerm, setSearchTerm] = useState<string>("");
 
   useEffect(() => {
     async function fetchGuildMembers() {
@@ -21,7 +26,7 @@ const GuildMembers: React.FC = () => {
         "https://klyuchik-v-durku-backend.herokuapp.com/guild-members"
       );
       const data = await response.json();
-      setMembers(data);
+      setMembers(data as GuildMember[]);
     }
 
     fetchGuildMembers();
@@ -33,33 +38,153 @@ const GuildMembers: React.FC = () => {
 
   const startIndex = (page - 1) * PAGE_SIZE;
   const endIndex = startIndex + PAGE_SIZE;
-  const displayedMembers = members.slice(startIndex, endIndex);
+  const filteredMembers = members.filter((member) =>
+    member.character.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+  const classFilteredMembers = classFilter
+    ? filteredMembers.filter(
+      (member) => member.character.class === classFilter
+    )
+    : filteredMembers;
+  const sortedMembers = classFilteredMembers.sort((a, b) => a.rank - b.rank);
+  const displayedMembers = sortedMembers.slice(startIndex, endIndex);
+  const groupedMembers = displayedMembers.reduce((acc, member) => {
+    if (!acc[member.rank]) {
+      acc[member.rank] = [];
+    }
+    acc[member.rank].push(member);
+    return acc;
+  }, {});
+
+  function getRankName(rank: string): string {
+    switch (rank) {
+      case "0":
+        return "ГМ";
+      case "1":
+        return "Зам ГМ";
+      case "2":
+        return "Альт Офицера";
+      case "3":
+        return "Резчик Лука";
+      case "4":
+        return "Статик";
+      case "5":
+        return "Завсегдатай";
+      case "6":
+        return "Стас";
+      case "7":
+        return "Местный";
+      case "8":
+        return "Альт";
+      case "9":
+        return "Посетитель";
+      default:
+        return "";
+    }
+  }
+  const handleSetClassFilter = useCallback((value) => {
+    setClassFilter(value);
+    setPage(1)
+  }, [])
+
+  const handleSetSearchValue = useCallback((value) => {
+    setSearchTerm(value);
+    setPage(1)
+  }, [])
+
+  function getClassColor(className: string): string {
+    switch (className) {
+      case "Warrior":
+        return "#C79C6E";
+      case 'Monk':
+        return '#00FF98';
+      case 'Evoker':
+        return '#33937F';
+      case 'Demon Hunter':
+        return '#A330C9';
+      case 'Death Knight':
+        return '#C41E3A';
+      case "Paladin":
+        return "#F58CBA";
+      case "Hunter":
+        return "#ABD473";
+      case "Rogue":
+        return "#FFF569";
+      case "Priest":
+        return "#FFFFFF";
+      case "Shaman":
+        return "#0070DE";
+      case "Mage":
+        return "#40C7EB";
+      case "Warlock":
+        return "#8787ED";
+      case "Druid":
+        return "#FF7D0A";
+      default:
+        return "#FFFFFF";
+    }
+  }
 
   return (
     <>
-      <List
-        grid={{ gutter: 16, column: 4 }}
-        header={<div>Состав гильдии</div>}
-        dataSource={displayedMembers}
-        renderItem={(member) => (
-          <List.Item>
-            <Card
-              cover={
-                <Avatar
-                  shape="circle"
-                  size={64}
-                  style={{ backgroundColor: "#87d068" }}
-                />
-              }
-            >
-              <Card.Meta
-                title={member.character.name}
-                description={`${member.character.active_spec_name} ${member.character.class}`}
-              />
-            </Card>
-          </List.Item>
-        )}
-      />
+      <div style={{ display: "flex", justifyContent: "flex-end", marginBottom: 16 }}>
+        <Input.Search
+          placeholder="Поиск по имени персонажа"
+          onChange={(e) => handleSetSearchValue(e.target.value)}
+          style={{ width: 300, marginTop: 20 }}
+        />
+        <Select
+          defaultValue=""
+          style={{ marginLeft: 16, width: 200, marginTop: 20 }}
+          onChange={(value) => handleSetClassFilter(value)}
+        >
+          <Option value="">Все классы</Option>
+          <Option value="Warrior">Воин</Option>
+          <Option value="Paladin">Паладин</Option>
+          <Option value="Hunter">Охотник</Option>
+          <Option value="Rogue">Разбойник</Option>
+          <Option value="Priest">Жрец</Option>
+          <Option value="Shaman">Шаман</Option>
+          <Option value="Mage">Маг</Option>
+          <Option value="Warlock">Чернокнижник</Option>
+          <Option value="Druid">Друид</Option>
+        </Select>
+      </div>
+      {Object.entries(groupedMembers as [GuildMember[]]).map(([rank, members]) => (
+        <div key={`rank-${rank}`}>
+          <h2> {getRankName(rank)}</h2>
+          <List
+            grid={{ gutter: 16, column: 4 }}
+            dataSource={members.filter((member) => {
+              const nameMatch = member.character.name
+                .toLowerCase()
+                .includes(searchTerm.toLowerCase());
+              const classMatch = classFilter
+                ? member.character.class === classFilter
+                : true;
+              return nameMatch && classMatch;
+            })}
+            renderItem={(member) => (
+              <List.Item>
+                <Card
+                  cover={
+                    <Avatar
+                      shape="circle"
+                      size={64}
+                      style={{ backgroundColor: getClassColor(member.character.class) }}
+                    />
+                  }
+                >
+                  <Card.Meta
+                    title={member.character.name}
+                    description={`${member.character.active_spec_name} ${member.character.class}`}
+                  />
+                </Card>
+              </List.Item>
+            )}
+          />
+        </div>
+      ))}
       <Pagination
         current={page}
         pageSize={PAGE_SIZE}
