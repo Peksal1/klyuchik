0805348fb590173c161014bf6cd1c50f1edb0276
Ilja Laurs <iljal@peplink.com>
2023-03-13 18:11:05 +0200

    hello

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -50,27 +50,64 @@ const AboutGuildContext = React.createContext<{
 const AboutGuild: React.FC = () => {
   const guildInfo = useContext(AboutGuildContext);
   return (
-    <Card title="О гильдии" style={{ maxWidth: 600, marginTop: "20px" }}>
-      <Row gutter={[16, 16]}>
-        <Col span={24}>
-          <Card>
-            <div>Название гильдии:</div>
-            <div>{guildInfo.name}</div>
-          </Card>
-        </Col>
-        <Col span={12}>
-          <Card>
-            <div>Сервер:</div>
-            <div>{guildInfo.server}</div>
-          </Card>
-        </Col>
-        <Col span={12}>
-          <Card>
-            <div>Фракция:</div>
-            <div>{guildInfo.faction}</div>
-          </Card>
-        </Col>
-      </Row>
+    <Card
+      title={<span style={{ fontSize: 24 }}>О гильдии</span>}
+      style={{ maxWidth: 800, margin: "20px auto" }}
+    >
+      <div style={{ fontSize: 20, fontWeight: "bold", marginBottom: 10 }}>
+        Гильдия{" "}
+        <span style={{ color: "#8c1616", fontFamily: "WarCraft" }}>
+          ⭐ Ключик в дурку ⭐
+        </span>{" "}
+        возобновляет игру в World of Warcraft и собирает состав единомышленников
+        для максимально позитивного времяпровождения как внутри игры (ключи,
+        казуальные рейды), так и вне.
+      </div>
+
+      <div style={{ marginBottom: 20 }}>
+        Мы хотим восстановить наше былое сообщество людей, которые любят эту
+        игру, и хорошо в ней проводить время так же, как и мы. Основная цель
+        гильдии - фарм ключей и интересное общение, с казуальным рейдингом в
+        рамках небольшого РТ.
+      </div>
+
+      <div style={{ fontWeight: "bold", marginBottom: 10 }}>
+        Что мы можем предложить?
+      </div>
+
+      <ul style={{ marginBottom: 20 }}>
+        <li>
+          Вы новичок/казуальный игрок? - Постараемся передать свои знания,
+          помочь в начинаниях
+        </li>
+        <li>
+          С более опытными игроками - было бы интересно пытаться взять р1
+          ключей, со стабильной игрой в обговоренное время
+        </li>
+        <li>
+          Казуальные рейды, 1-2 дня в неделю, пт+суб 20 00 - 23 00 мск. Цель -
+          фармить героик и легких боссов мифика мейнами/альтами
+        </li>
+        <li>
+          Открытые логи. записи рейдов/ивентов, котлы, руны, еда, ремонт.
+          Атмосфера добра и счастья. Закрываем всем рейдерам ключи.
+        </li>
+        <li>Совместное выполнение достижений вроде "Слава рейдеру ..."</li>
+        <li>Внутриигровая помощь другим игрокам за внутриигровую валюту</li>
+      </ul>
+      <div style={{ fontWeight: "bold", marginBottom: 10 }}>Наши контакты:</div>
+      <div style={{ marginBottom: 10 }}>
+        <span style={{ marginRight: 5 }}>Discord (Канал гильдии):</span>
+        <a href="https://discord.gg/fjVkeJaKjc">Ключик в дурку</a>
+      </div>
+      <div style={{ marginBottom: 10 }}>
+        <span style={{ marginRight: 5 }}>Discord (Представители):</span>
+        <span>Peksal1#0974</span>
+      </div>
+      <div>
+        <span style={{ marginRight: 5 }}>В игре (Представители):</span>
+        <span>Хамингодк, Хамингвуд, Бродерман</span>
+      </div>
     </Card>
   );
 };
