20d9d2b99414e1cf5bf8fb0acdb71d0a118a9442
Ilja Laurs <iljal@peplink.com>
2023-03-17 16:29:49 +0200

    Redirect http to https

--- src/Header.tsx
+++ src/Header.tsx
@@ -6,6 +6,17 @@ import "./styles/Header.css";
 
 const { Header } = Layout;
 
+const express = require("express");
+const app = express();
+
+// Add this middleware to redirect HTTP to HTTPS
+app.use((req, res, next) => {
+  if (req.headers["x-forwarded-proto"] !== "https") {
+    return res.redirect("https://" + req.headers.host + req.url);
+  }
+  return next();
+});
+
 function AppHeader() {
   return (
     <Header className="header">
