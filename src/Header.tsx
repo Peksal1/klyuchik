b1cdef1e028c43355042b682a35832e149e5ad50
Ilja Laurs <iljal@peplink.com>
2023-03-21 14:06:45 +0200

    bnet info test

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,12 +1,28 @@
 import { Avatar, Layout } from "antd";
 import Title from "antd/es/typography/Title";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import LoginPopover from "./components/loginPopover/index.tsx";
 import "./styles/Header.css";
 
 const { Header } = Layout;
 
 function AppHeader() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch("/profile");
+        const userData = await response.json();
+        setUser(userData);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+
+    fetchData();
+  }, []);
+
   return (
     <Header className="header">
       <div className="logo-container">
@@ -22,7 +38,13 @@ function AppHeader() {
           Ключик в Дурку
         </Title>
       </div>
-      <LoginPopover />
+      {user ? (
+        <div style={{ color: "white", marginLeft: "auto" }}>
+          {user.battletag}
+        </div>
+      ) : (
+        <LoginPopover />
+      )}
     </Header>
   );
 }
