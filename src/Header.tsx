35cfa000a047ea5c56dcaa9bd04419dbd6b78229
Ilja Laurs <iljal@peplink.com>
2023-03-21 14:29:05 +0200

    Fix api call

--- src/Header.tsx
+++ src/Header.tsx
@@ -13,7 +13,7 @@ function AppHeader() {
   useEffect(() => {
     // Make API call to retrieve user's Battle.net profile information
     axios
-      .get("/bnet/profile")
+      .get("https://klyuchik-v-durku-backend.herokuapp.com/bnet/profile")
       .then((response) => {
         // Set the user's Battle.net profile name in state
         setBnetProfileName(response.data.name);
